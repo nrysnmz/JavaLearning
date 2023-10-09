7647cba61aae7ebe00fbd8e7d7ae5e576f85f61e
Nuray Korganci <nrysnmz@gmail.com>
2023-10-08 21:35:37 -0500

    md

---
+++ src/main/java/HeapStack.java
@@ -0,0 +1,31 @@
+
+class Calculator {
+    int num = 5;
+
+    public int add(int n1, int n2, int n3) {
+        System.out.println(num);
+        return n1 + n2 + n3;
+    }
+
+    public int add(int n1, int n2) {
+        return n1 + n2;
+    }
+
+    public double add(double n1, int n2) {
+        return n1 + n2;
+    }
+
+}
+
+public class HeapStack {
+
+    public static void main(String[] args) {
+        Calculator obj = new Calculator();
+        Calculator obj1 = new Calculator();
+        obj.num = 8;
+        System.out.println(obj.num);
+        System.out.println(obj1.num);
+    }
+
+}
+
---
+++ src/main/java/ObjectCreation.java
@@ -0,0 +1,28 @@
+public class ObjectCreation {
+
+   static int a = 2;
+
+
+    public static void add(int number1, int number2) {
+        int sum = number1 + number2;
+    }
+
+    public void sample(){
+      a =4;
+
+    }
+
+    public static void c(){
+       ObjectCreation.a =4;
+       add(5,45);
+    }
+
+}
+
+class Calculations {
+
+    public void a(){
+
+
+    }
+}
